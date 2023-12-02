ae7030cb5c991c355a5f271e19f597947d51468b
denx24x <d.belozorov1@gmail.com>
2023-12-02 17:32:49 +0300

    added integration tests

--- origination/src/test/java/com/academy/fintech/origination/BlackBoxTest.java
+++ origination/src/test/java/com/academy/fintech/origination/BlackBoxTest.java
@@ -1,15 +1,19 @@
 package com.academy.fintech.origination;
 
+import com.academy.fintech.application.*;
+import com.academy.fintech.origination.core.application.DuplicateApplicationException;
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
 
 
 @Testcontainers
@@ -32,5 +36,56 @@ public class BlackBoxTest {
                 .GET().build();
         String result = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
         Assertions.assertEquals(result, "{\"status\":\"UP\"}");
+    }
+
+    @Test
+    void testApplication() {
+        Channel channel = ManagedChannelBuilder.forAddress("host.docker.internal", 9094).usePlaintext().build();
+        ApplicationServiceGrpc.ApplicationServiceBlockingStub stub = ApplicationServiceGrpc.newBlockingStub(channel);
+        ApplicationResponse applicationResponse = stub.create(ApplicationRequest.newBuilder()
+                .setEmail("123@123.ru")
+                .setFirstName("Andrew")
+                .setLastName("Borisov")
+                .setDisbursementAmount("123.123")
+                .build());
+        Assertions.assertTrue(applicationResponse.getApplicationId() >= 0);
+    }
+
+    @Test
+    void testApplicationDuplicate() {
+        Channel channel = ManagedChannelBuilder.forAddress("host.docker.internal", 9094).usePlaintext().build();
+        ApplicationServiceGrpc.ApplicationServiceBlockingStub stub = ApplicationServiceGrpc.newBlockingStub(channel);
+        ApplicationResponse applicationResponse = stub.create(ApplicationRequest.newBuilder()
+                .setEmail("123@123.ru")
+                .setFirstName("Andrew")
+                .setLastName("Borisov")
+                .setDisbursementAmount("123.123")
+                .build());
+        DuplicateApplicationException exception = Assertions.assertThrows(DuplicateApplicationException.class,
+                () -> {
+                    stub.create(ApplicationRequest.newBuilder()
+                            .setEmail("123@123.ru")
+                            .setFirstName("Andrew")
+                            .setLastName("Borisov")
+                            .setDisbursementAmount("123.123")
+                            .build());
+                });
+    }
+
+    @Test
+    void testCancel() {
+        Channel channel = ManagedChannelBuilder.forAddress("host.docker.internal", 9094).usePlaintext().build();
+        ApplicationServiceGrpc.ApplicationServiceBlockingStub stub = ApplicationServiceGrpc.newBlockingStub(channel);
+        ApplicationResponse applicationResponse = stub.create(ApplicationRequest.newBuilder()
+                .setEmail("123@123.ru")
+                .setFirstName("Andrew")
+                .setLastName("Borisov")
+                .setDisbursementAmount("123.123")
+                .build());
+        CancelApplicationResponse cancelApplicationResponse = stub.cancel(
+                CancelApplicationRequest.newBuilder()
+                        .setApplicationId(applicationResponse.getApplicationId()).build()
+        );
+        Assertions.assertTrue(cancelApplicationResponse.getSuccess());
     }
 }
