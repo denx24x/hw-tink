2709e1dcf6959f26e8e50d6bda65ce28d167bf1d
denx24x <d.belozorov1@gmail.com>
2023-12-07 00:26:35 +0300

    init

---
+++ scoring/src/main/java/com/academy/fintech/scoring/Application.java
@@ -0,0 +1,31 @@
+package com.academy.fintech.scoring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.netty.NettyAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
+
+@SpringBootApplication(
+        exclude = {
+                GsonAutoConfiguration.class,
+                MultipartAutoConfiguration.class,
+                WebSocketServletAutoConfiguration.class,
+                NettyAutoConfiguration.class,
+                DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class
+        }
+)
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class)
+                .beanNameGenerator(new FullyQualifiedAnnotationBeanNameGenerator())
+                .run(args);
+    }
+
+}
